e74ad64355a69ff3a25c3312e88d30e6f432946b
Profesor Bastar <Profesor Bastar@DESKTOP-JPSLELM>
2016-12-16 00:27:39 -0600

    Menu elaborado

--- ProyectoBachiGit/src/GUI/PrincipalProducto.java
+++ ProyectoBachiGit/src/GUI/PrincipalProducto.java
@@ -6,26 +6,26 @@
 package GUI;
 
 import JDBC.EmpleadoJDBC;
-import POJO.EmpleadoPOJO;
-import java.math.MathContext;
+import JDBC.ProductoJDBC;
+import POJO.ProductoPOJO;
 import javax.swing.JOptionPane;
 
 /**
  *
  * @author Profesor Bastar
  */
-public class PrincipalEmpleado extends javax.swing.JFrame {
+public class PrincipalProducto extends javax.swing.JFrame {
 
     /**
-     * Creates new form PrincipalEmpleado
+     * Creates new form PrincipalProducto
      */
-    public PrincipalEmpleado() {
+    public PrincipalProducto() {
         initComponents();
         cargarTabla();
     }
 
     private void cargarTabla() {
-        jTable1.setModel(EmpleadoJDBC.cargarTabla());
+        jTable1.setModel(ProductoJDBC.cargarTabla());
     }
 
     /**
@@ -39,7 +39,11 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
 
         jDialog1 = new javax.swing.JDialog();
         jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -50,14 +54,18 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
 
         jLabel1.setText("Nombre");
 
-        jButton3.setText("Guardar");
+        jLabel2.setText("Costo");
+
+        jLabel3.setText("Existencia");
+
+        jButton3.setText("Cancelar");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
             }
         });
 
-        jButton4.setText("Cancelar");
+        jButton4.setText("Guardar");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton4ActionPerformed(evt);
@@ -71,29 +79,43 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
             .addGroup(jDialog1Layout.createSequentialGroup()
                 .addGroup(jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jDialog1Layout.createSequentialGroup()
-                        .addGap(20, 20, 20)
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(33, 33, 33)
+                        .addGroup(jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(47, 47, 47)
+                        .addGroup(jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jDialog1Layout.createSequentialGroup()
                         .addGap(52, 52, 52)
-                        .addComponent(jButton3)
-                        .addGap(56, 56, 56)
-                        .addComponent(jButton4)))
-                .addContainerGap(130, Short.MAX_VALUE))
+                        .addComponent(jButton4)
+                        .addGap(44, 44, 44)
+                        .addComponent(jButton3)))
+                .addContainerGap(90, Short.MAX_VALUE))
         );
         jDialog1Layout.setVerticalGroup(
             jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jDialog1Layout.createSequentialGroup()
-                .addGap(77, 77, 77)
+                .addGap(56, 56, 56)
                 .addGroup(jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
-                .addGap(55, 55, 55)
+                .addGap(18, 18, 18)
                 .addGroup(jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton3)
-                    .addComponent(jButton4))
-                .addContainerGap(47, Short.MAX_VALUE))
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(107, 107, 107)
+                .addGroup(jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton4)
+                    .addComponent(jButton3))
+                .addContainerGap(141, Short.MAX_VALUE))
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -139,60 +161,81 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+                .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addComponent(jButton5))
-                .addGap(44, 44, 44))
+                .addContainerGap(60, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
             .addGroup(layout.createSequentialGroup()
-                .addGap(94, 94, 94)
-                .addComponent(jButton1)
-                .addGap(18, 18, 18)
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton5)
-                .addGap(115, 115, 115))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(162, 162, 162)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2)
+                        .addGap(55, 55, 55)
+                        .addComponent(jButton5)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        String nombre = jTextField1.getText();
-        EmpleadoPOJO pojo = new EmpleadoPOJO();
-        pojo.setNombre(nombre);
-        boolean x = EmpleadoJDBC.insertar(pojo);
-        if (x == true) {
-            JOptionPane.showMessageDialog(null, "Éxito");
-            jDialog1.setVisible(false);
-            cargarTabla();
-            limpiaTF();
-        } else {
-            JOptionPane.showMessageDialog(null, "Error");
-        }
+        jDialog1.setVisible(false);
+        limpiaTF();
 // TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
-    private void limpiaTF() {
-        jTextField1.setText(null);
-    }
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        jDialog1.setVisible(true);
-        jDialog1.setSize(400, 400);        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        jDialog1.setVisible(false);
-        limpiaTF();// TODO add your handling code here:
+        String n = jTextField1.getText();
+        String c = jTextField2.getText();
+        String ex = jTextField3.getText();
+        double costo = 0;
+        double existencia = 0;
+
+        try {
+            costo = Double.parseDouble(c);
+            existencia = Double.parseDouble(ex);
+            ProductoPOJO pojo = new ProductoPOJO();
+            pojo.setCosto(costo);
+            pojo.setNombre(n);
+            pojo.setExistencia(existencia);
+            boolean x = ProductoJDBC.insertar(pojo);
+            if (x == true) {
+                JOptionPane.showMessageDialog(null, "Éxito");
+                jDialog1.setVisible(false);
+                cargarTabla();
+                limpiaTF();
+            } else {
+                JOptionPane.showMessageDialog(null, "Error ");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error en conversión");
+        }
+
+// TODO add your handling code here:
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        jDialog1.setVisible(true);
+        jDialog1.setSize(400, 400);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+        setVisible(false);
+        new Menu().setVisible(true);
+    }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         int fi = jTable1.getSelectedRow();
@@ -200,7 +243,7 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Primero selecciona una fila");
         } else {
             String id = jTable1.getValueAt(fi, 0).toString();
-            boolean x = EmpleadoJDBC.eliminar(id);
+            boolean x = ProductoJDBC.eliminar(id);
             if (x == true) {
                 JOptionPane.showMessageDialog(null, "Exito");
                 cargarTabla();
@@ -211,11 +254,11 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        this.setVisible(false);
-        new Menu().setVisible(true);
-// TODO add your handling code here:
-    }//GEN-LAST:event_jButton5ActionPerformed
+    private void limpiaTF() {
+        jTextField1.setText(null);
+        jTextField2.setText(null);
+        jTextField3.setText(null);
+    }
 
     /**
      * @param args the command line arguments
@@ -234,20 +277,20 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PrincipalEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PrincipalProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PrincipalEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PrincipalProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PrincipalEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PrincipalProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PrincipalEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PrincipalProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PrincipalEmpleado().setVisible(true);
+                new PrincipalProducto().setVisible(true);
             }
         });
     }
@@ -260,8 +303,12 @@ public class PrincipalEmpleado extends javax.swing.JFrame {
     private javax.swing.JButton jButton5;
     private javax.swing.JDialog jDialog1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
--- ProyectoBachiGit/src/JDBC/ProductoJDBC.java
+++ ProyectoBachiGit/src/JDBC/ProductoJDBC.java
@@ -5,12 +5,18 @@
  */
 package JDBC;
 
+import POJO.ProductoPOJO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Profesor Bastar
  */
 public class ProductoJDBC {
-    
+
     private static final String TABLE = "Producto";
 
     private static final String SQL_INSERT = "Insert into " + TABLE
@@ -22,6 +28,83 @@ public class ProductoJDBC {
 
     private static final String SQL_QUERY = "Select * from " + TABLE;
 
-    
-    
+    public static boolean insertar(ProductoPOJO pojo) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        int x;
+        try {
+            con = Conexion.getConnection();
+            ps = con.prepareStatement(SQL_INSERT);
+            ps.setString(1, pojo.getNombre());
+            ps.setDouble(2, pojo.getExistencia());
+            ps.setDouble(3, pojo.getCosto());
+
+            x = ps.executeUpdate();//Aquí se ejecuta la inserción
+            if (x == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("Error al insertar " + e);
+            return false;
+        } finally {
+            Conexion.close(con);
+            Conexion.close(ps);
+        }
+    }
+
+    public static boolean eliminar(String id) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        int x;
+        try {
+            con = Conexion.getConnection();//Obtengo la conexion a la bd
+            ps = con.prepareStatement(SQL_DELETE);//Preparo el insert
+            ps.setString(1, id);
+            x = ps.executeUpdate();
+            if (x == 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Error al eliminar " + e);
+            return false;
+        } finally {
+            Conexion.close(con);
+            Conexion.close(ps);
+        }
+        return true;
+    }
+
+    public static DefaultTableModel cargarTabla() {
+        Connection con = null;
+        PreparedStatement ps = null;
+        DefaultTableModel modelo = null;
+        //Como me gustaría que salieran 
+        String encabezados[] = {"ID", "Nombre", "Existencia", "Costo"};
+        try {
+            con = Conexion.getConnection();
+            ps = con.prepareStatement(SQL_QUERY);
+            modelo = new DefaultTableModel();
+            modelo.setColumnIdentifiers(encabezados);//Aqui asigno los encabe
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Object ob[] = new Object[4];//Número de campos
+                //A partir de aquí son IDENTICOS a la BD
+                ob[0] = rs.getObject("idProducto");
+                ob[1] = rs.getObject("nombre");
+                ob[2] = rs.getObject("existencia");
+                ob[3] = rs.getObject("costo");
+                modelo.addRow(ob);
+            }
+            Conexion.close(rs);
+        } catch (Exception e) {
+            System.out.println("Error al consultar " + e);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(ps);
+        }
+        return modelo;
+    }
+
 }
